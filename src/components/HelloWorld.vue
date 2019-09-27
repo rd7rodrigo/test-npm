7f13746dfa50c22cd99d84fa785b95e9fb424129
rd7.rodrigo <rd7.rodrigo@gmail.com>
2019-09-27 01:53:21 -0300

    enviando

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <Banner>Este é o banner!</Banner>
+    <Button>ENTRAR</Button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { default as Banner } from './components/Banner.vue';
+export { default as Button } from './components/Button.vue';
